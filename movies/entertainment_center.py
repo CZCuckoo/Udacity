eb72f1f75f7118ef120fc5115d785afd37f43eda
Clint Johnson <Clint.Johnson.07@gmail.com>
2017-08-02 15:19:06 -0400

    Add files via upload

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,17 +1,20 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://youtu.be/d1_JBMrrYw8")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg", "https://youtu.be/d1_JBMrrYw8")
+force_awakens = media.Movie("Star Wars: The Force Awakens", "A young woman discovers she is strong in the force.", "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg", "https://youtu.be/sGbxmsDFVnE")
 
-force_awakens = media.Movie("Star Wars: The Force Awakens", "A young woman discovers she is strong in the force.", "https://en.wikipedia.org/wiki/Star_Wars:_The_Force_Awakens#/media/File:Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg", "https://youtu.be/sGbxmsDFVnE") 
+aliens = media.Movie("Aliens", "A team of marines discover investigate a planet and get more than they anticipated", "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg", "https://youtu.be/W857ys3BlRI")
 
-print(avatar.storyline)
+princess_bride = media.Movie("The Princess Bride", "A young child, home from school, bonds with his grandfather as he is read a story", "https://upload.wikimedia.org/wikipedia/en/d/db/Princess_bride.jpg", "https://youtu.be/VYgcrny2hRs")
 
-#avatar.show_trailer()
+arrival = media.Movie("The Arrival", "A translator must quickly learn an alien language to stop violence.", "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg", "https://youtu.be/tFMo3UJ4B4g")
 
-print(force_awakens.storyline)
 
-force_awakens.show_trailer()
+
+movies = [toy_story, avatar, force_awakens, aliens, princess_bride, arrival]
+
+fresh_tomatoes.open_movies_page(movies)
